c412923ee48c99bcfc93aaf224f08223449e6d30
iChady <2145527@slu.edu.ph>
2019-04-06 09:20:27 +0800

    Added directory for new UI

--- js/cors.js
+++
@@ -1,8 +0,0 @@
-var xhr = new XMLHttpRequest()
-        xhr.open('POST', '../Data/FetchData', true)
-        xhr.withCredentials = true
-        xhr.onreadystatechange = function() {
-          if (xhr.readyState === 2) {// do something}
-        }
-        xhr.setRequestHeader('Content-Type', 'application/json')
-        xhr.send(json)
