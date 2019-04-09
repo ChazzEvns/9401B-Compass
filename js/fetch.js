dbb37d0c2f352d1a8f0c40c53282daf933604827
Chazzevns <2165731@slu.edu.ph>
2019-04-09 15:22:50 +0800

    categories are already displaying data but view buttons are yet to be finished. I would like to request help for this issue for us to be finished quickly. thank you very much.

--- js/fetch.js
+++ js/fetch.js
@@ -1,13 +1,45 @@
-new Vue({
+//var data = { c: 1 } TB
+
+ new Vue({
+    //data: data TB
     el: '#app',
     data () {
       return {
-        info: null
+        info: []
       }
     },
     mounted () {
+    // API Endpoint
       axios
-        .get('http://api.eventful.com/json/events/search?keywords=music&location=Singapore&app_key=9KM6xpqMgc6RqHPj')
-        .then(response => (this.info = response))
-    }
+        .get('https://www.eventbriteapi.com/v3/categories/?token=BI4EZ4WPLG53F6RLTG7B')
+        .then(response => (this.info = response.data.categories))
+        .catch(error => console.log(error))
+    },
+     
+    
+    
+    
   });
+/*
+var Component = Vue.extend ({
+    data: function () {
+        return {c: 1};
+    }
+})*/
+/*
+new Vue({
+    el: '#fetch',
+    data () {
+      return {
+        //info: null,
+        results: null
+      }
+    },
+    
+    mounted () {
+      axios
+        .get('http://api.eventful.com/rest/categories/list?subcategories=music&app_key=9KM6xpqMgc6RqHPj')
+        .then(response => (this.results = response))
+        .catch(error => console.log(error))
+    }
+  });*/
--- readme.txt
+++
@@ -1,6 +0,0 @@
-Incase there are errors in console:
-
-1. install Allow Control Allow Origin chrome extension
-
-2. change properties of google chrome
-	2.1 Target: "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --disable-web-security --user-data-dir="[path of specific directory for command]"
