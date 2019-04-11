f50e426363fafdf7025e1d6b228a8a2caeec326a
Chazzevns <2165731@slu.edu.ph>
2019-04-12 02:27:48 +0800

    The final phase of categories.html(although not complete)

--- js/fetch.js
+++ js/fetch.js
@@ -43,7 +43,10 @@ function fetch_categories() {
 			for(var i = 0; i < category_list.length; i++) {
 				var category = category_list[i];
 
-				content += "<div class="+category.short_name+">"+category.name+"</div>";
+				content += "<div class="+category.short_name+">"
+                    +"<h2>"+category.name+"</h2>"
+                    +"<div class="+"button"+">"+"<a href="+"https://www.eventbriteapi.com/v3/events/search/?categories="+category.id+"&"+"token=BI4EZ4WPLG53F6RLTG7B"+">"+"View"+"</a>"+"</div>"
+                    +"</div>";
 			}
 			
 			categories.html(content);
